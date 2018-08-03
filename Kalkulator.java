d8a7d7ab68a787a454f17696aee27514f549a892
ElmedinEdo <edohanic@hotmail.com>
2018-08-03 20:32:23 +0200

    Kalkulator

---
+++ Kalkulator.java
@@ -0,0 +1,19 @@
+
+public class Kalkulator {
+
+		
+    public void sabiranje(int a , int b) {
+    
+    
+	}
+    public void duzimanje(int a, int b) {
+	}
+
+    
+    public void mnozenje(int a , int b) {
+	}
+  
+    public void djeljenje(int a , int b) {
+	}
+
+}
